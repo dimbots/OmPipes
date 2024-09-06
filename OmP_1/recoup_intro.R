4e4af11d452a3429540dea1ac8d92a6277fc0b31
Dimbo <dim.botskaris@gmail.com>
2024-09-06 15:01:20 +0300

    add

---
+++ OmP_1/recoup_intro.R
@@ -0,0 +1,16 @@
+## ----echo=TRUE, eval=TRUE, message=FALSE--------------------------------------
+library(recoup)
+help(recoup)
+
+## ----echo=TRUE, eval=TRUE, message=FALSE--------------------------------------
+help(test.input)
+help(test.genome)
+help(test.design)
+help(test.exons)
+
+## ----echo=TRUE, eval=FALSE----------------------------------------------------
+#  buildAnnotationDatabase(c("hg19","mm9","rn4"),c("ensembl","refseq"))
+
+## ----echo=FALSE---------------------------------------------------------------
+sessionInfo()
+
